6dc7c2705bf4b61f4c50cc78a2785c89d413c21a
badrelkhiri <elkhiribadr@gmail.com>
2025-03-26 02:42:49 +0000

    upd

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,10 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    historyApiFallback: true,
-  },
+  base: './',
 });
